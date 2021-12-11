1c41710fb73087eef275fe1a896a005d8c1ca3c1
hquar <95965016+hquar@users.noreply.github.com>
2021-12-11 17:13:30 +0530

    Update uninstall.sh
    Added crackmapexec to the list

--- uninstall.sh
+++ uninstall.sh
@@ -132,6 +132,7 @@ printf 'y' |sudo apt remove zaproxy
 printf 'y' |sudo apt remove freerdp2-x11
 printf 'y' |sudo apt remove scalpel
 printf 'y' |sudo apt remove spiderfoot
+printf 'y' |sudo apt remove crackmapexec
 printf 'y' |sudo apt autoclean 
 printf 'y' |sudo apt autoremove
 printf 'y' |sudo apt update 
